ebc98ce6b04478bf371f00e8bc78f37e8a2ed974
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-08 19:49:37 -0500

    Add /urls/new route to get and render urls_new template page

--- express_server.js
+++ express_server.js
@@ -22,6 +22,10 @@ app.get('/urls', (req,res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render("urls_new");
+} )
+
 app.get('/urls/:shortURL', (req,res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
