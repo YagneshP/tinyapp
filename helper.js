92c5cc4fe36da1896959636e96aa407520588367
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-11 17:50:50 -0500

    Modulize the helper function in helper.js and exporting it

---
+++ helper.js
@@ -0,0 +1,9 @@
+function checkUserWithEmail(email, db) {
+  for(let user in db) {
+    if(db[user]['email'] === email) {
+      return db[user];
+    }
+  }
+}
+
+module.exports = checkUserWithEmail;
