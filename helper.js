7a2b38bc97fe61314abab07251f6f1e82ae8d461
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-11 20:12:52 -0500

    Fix lint error

--- helper.js
+++ helper.js
@@ -1,19 +1,19 @@
-function checkUserWithEmail(email, db) {
-  for(let user in db) {
-    if(db[user]['email'] === email) {
+const checkUserWithEmail = function(email, db) {
+  for (let user in db) {
+    if (db[user]["email"] === email) {
       return db[user];
     }
   }
-}
+};
 
-function urlsForUser(id, db) {
+const urlsForUser = function(id, db) {
   let obj = {};
   for (let key in db) {
-    if (db[key]['userID'] === id) {
+    if (db[key]["userID"] === id) {
       obj[key] = db[key];
     }
   }
   return obj;
-}
+};
 
-module.exports = {checkUserWithEmail,urlsForUser};
+module.exports = { checkUserWithEmail, urlsForUser };
