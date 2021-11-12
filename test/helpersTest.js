a7c92ecb2165a981e58edb6b7bec6236ea747c4e
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-11 20:14:19 -0500

    Fix lint error

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,28 +1,28 @@
-const { assert } = require('chai');
+const { assert } = require("chai");
 
-const { checkUserWithEmail } = require('../helper.js');
+const { checkUserWithEmail } = require("../helper.js");
 
 const testUsers = {
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
   },
-  "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
-  }
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
 };
 
-describe('getUserByEmail', function() {
-  it('should return a user with valid email', function() {
-    const user = checkUserWithEmail("user@example.com", testUsers)
+describe("getUserByEmail", function() {
+  it("should return a user with valid email", function() {
+    const user = checkUserWithEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
-    assert.equal(user['id'], expectedUserID);
+    assert.equal(user["id"], expectedUserID);
   });
-  it('should return undefined with invalid email', function() {
-    const user = checkUserWithEmail("test@test.com", testUsers)
-    assert.isUndefined(user, 'user should be undefined with invalid email');
+  it("should return undefined with invalid email", function() {
+    const user = checkUserWithEmail("test@test.com", testUsers);
+    assert.isUndefined(user, "user should be undefined with invalid email");
   });
 });
